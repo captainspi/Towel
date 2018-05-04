3d563d8fa987a9543600f91bac6c7b8875922176
Omar Ansari <omar.ansari.wt@gmail.com>
2018-05-04 23:51:21 +0200

    :#16 New test to check the actual contents in the RomanNumeralsBag

--- tests/unit/Converter/testRomanNumeralsBag.py
+++ tests/unit/Converter/testRomanNumeralsBag.py
@@ -5,8 +5,15 @@ class Test(unittest.TestCase):
     """ Tests the RomanNumeralsBag"""
 
     def test_equals(self):
-        """Tests the basic insert operation of the Bag"""
-        #Set Up
+        """Tests the output of the actual list entries of the Bag"""
+        roman_numerals_bag = RomanNumeralsBag()
+        roman_numerals_bag.append('X')
+        self.assertEqual(roman_numerals_bag.numeralsList, ['X'])
+        roman_numerals_bag.append('L')
+        self.assertEqual(roman_numerals_bag.numeralsList, ['X', 'L'])
+
+    def test_stringify(self):
+        """Tests the output after performing basic insert operation on the Bag"""
         roman_numerals_bag = RomanNumeralsBag()
         roman_numerals_bag.append('X')
         self.assertEqual(str(roman_numerals_bag), 'X')
