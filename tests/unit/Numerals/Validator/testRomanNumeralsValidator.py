13a1d7edf6107441d1194e00379f0220223b9e5a
Omar Ansari <omar.ansari.wt@gmail.com>
2018-05-07 00:10:54 +0200

    #15 adding test for basic set of roman numerals

---
+++ tests/unit/Numerals/Validator/testRomanNumeralsValidator.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+
+class TestRomanNumeralsValidator(TestCase):
+    """"Tests the Validator for RomanNumerals"""
+
+    def testValidRomanNumeralsSet(self):
+        """Tests the basic set of roman numerals for validity"""
+        data_provider = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+        roman_numerals_validator = RomanNumeralsValidator()
+        for test_number in range(data_provider.__len__()):
+            with self.subTest(i=test_number):
+                self.assertTrue(roman_numerals_validator.validate(data_provider.pop()), True)
